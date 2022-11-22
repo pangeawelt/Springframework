78304a5110fc1c41dd6f1ab66ed30bce647664b5
ganiebuer <72691811+pangeawelt@users.noreply.github.com>
2022-11-22 19:56:39 +0100

    Add files via upload

---
+++ verbindungpostgresql/src/main/java/de/springwebseite/verbindungpostgresql/VerbindungpostgresqlApplication.java
@@ -0,0 +1,13 @@
+package de.springwebseite.verbindungpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbindungpostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbindungpostgresqlApplication.class, args);
+	}
+
+}
---
+++ verbindungpostgresql/src/test/java/de/springwebseite/verbindungpostgresql/VerbindungpostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.springwebseite.verbindungpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbindungpostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
